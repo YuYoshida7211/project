806eac30dbb6bc8935d62e1fd587acbbf75146fa
Yoshida Yu <yuyoshida721100@gmail.com>
2020-01-03 15:26:38 +0900

    typescript環境削除

--- apps/webroot/src/actions/Actions.ts
+++
@@ -1,8 +0,0 @@
-import actionCreatorFactory from 'typescript-fsa';
-
-const actionCreator = actionCreatorFactory();
-
-export const Actions = {
-    Action1: actionCreator<string>('ACTIONS_1'),
-    ACtion2: actionCreator<string>('ACTIONS_2')
-};
