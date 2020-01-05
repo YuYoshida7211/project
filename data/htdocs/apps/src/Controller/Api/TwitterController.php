692071b46aa22675e2941fb3ad0feb485230904b
橋本歩実 <ayumi_hashimoto@respawn.jp>
2020-01-05 16:38:47 +0900

    日時バッチと日時バッチで呼び出すcontroller修正しました！ appsでbin/cake api でshellが回ってresult.jsonが作られます！

--- data/htdocs/apps/src/Controller/Api/TwitterController.php
+++ data/htdocs/apps/src/Controller/Api/TwitterController.php
@@ -15,44 +15,53 @@ class twitterController extends Controller
 
     public function index($mode = null) {
 
-      // twitterのコネクション生成に必要な情報を設定
-      $consumerKey = "MdCFvSj79eUE0uWPzlfM1cSyH";
-      $consumerSecret = "P22a0RP9mDy4nVyDueMb6vYSrbodc3dttwXAWJTC0ODrU0TOQT";
-      $accessToken = "1118130091672735744-AKjMsdSOTkNRRsDkVjurLv3SZiHKOG";
-      $accessTokenSecret = "BXGy6jMWRmCpcbRBnUmclrhOlkfsHojun3uJ6kFAG1ce5";
+        // twitterのコネクション生成に必要な情報を設定
+        $consumerKey = "MdCFvSj79eUE0uWPzlfM1cSyH";
+        $consumerSecret = "P22a0RP9mDy4nVyDueMb6vYSrbodc3dttwXAWJTC0ODrU0TOQT";
+        $accessToken = "1118130091672735744-AKjMsdSOTkNRRsDkVjurLv3SZiHKOG";
+        $accessTokenSecret = "BXGy6jMWRmCpcbRBnUmclrhOlkfsHojun3uJ6kFAG1ce5";
 
-      // コネクションを生成
-      $connection = new TwitterOAuth ( $consumerKey, $consumerSecret, $accessToken, $accessTokenSecret );
+        // コネクションを生成
+        $connection = new TwitterOAuth ( $consumerKey, $consumerSecret, $accessToken, $accessTokenSecret );
 
-      // トレンドワード取得
-      $request = $connection->OAuthRequest(
-          "https://api.twitter.com/1.1/trends/place.json","GET",array("id"=>"1118370")
-      ); //idは地域を表している　1118370＝東京　1118550=横浜
-      $json_array = json_decode($request,true);
-      $this->set('json',$request);
-      $this->set(compact('json_array'));
+        // トレンドワード取得
+        $request = $connection->OAuthRequest(
+            "https://api.twitter.com/1.1/trends/place.json","GET",array("id"=>"1118370")
+        ); //idは地域を表している　1118370＝東京　1118550=横浜
+        $json_array = json_decode($request,true);
 
-      for ($i = 0; $i < 1; $i++){
-        $trend_word = $json_array[0]['trends'][$i]['name'];
-        // キーワード検索してtweetを取得
-        $statuses = $connection->get ( 'search/tweets', array (
-        'lang' => 'ja',
-        'q' => $trend_word,
-        'result_type' => 'recent', // 人気投稿
-        'count' => '10'
-        ) );
+        $trend_posts = array();
+        for ($i = 0; $i < 10; $i++){
+            $trend_word = $json_array[0]['trends'][$i]['name'];
 
-        $posts = array();
-        foreach ($statuses->statuses as $key => $value) {
-            array_push($posts , $value);
+            // キーワード検索してtweetを取得する
+            $statuses = $connection->get ( 'search/tweets', array (
+            'lang' => 'ja',
+            'q' => $trend_word,
+            'result_type' => 'recent', // 最新投稿
+            'tweet_mode' => 'extended', //全文取得
+            'count' => '10'
+            ) );
+            $posts = array();
+            foreach ($statuses->statuses as $key => $value) {
+                $post = array();
+                $post['created_at'] = $value->created_at;
+                $post['tweet_id'] = $value->id;
+                $post['text'] = $value->full_text;
+                $post['media'] = isset($value->entities->media) ? $value->entities->media : null;
+                $post['user_name'] = $value->user->name;
+                $post['user_screen_name'] = $value->user->screen_name;
+                $post['profile_image_url'] = $value->user->profile_image_url;
+                array_push($posts , $post);
+            }
 
+            $trend_post["trend_word"] = $trend_word;
+            $trend_post["items"] = $posts;
+            array_push($trend_posts , $trend_post);
         }
-      }
 
-      $this->set([
-          'posts' => $posts,
-          '_serialize' => ['posts']
-      ]);
+        // ApiShellにjsonを返す
+        return json_encode($trend_posts, JSON_UNESCAPED_UNICODE|JSON_PRETTY_PRINT);
 
     }
 }
