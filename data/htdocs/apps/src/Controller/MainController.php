eea134400f7659108e563cdced0c32371a00a42b
橋本歩実 <ayumi_hashimoto@respawn.jp>
2020-01-28 22:25:22 +0900

    MainController

---
+++ data/htdocs/apps/src/Controller/MainController.php
@@ -0,0 +1,15 @@
+<?php
+namespace App\Controller;
+
+use Cake\Controller\Controller;
+
+
+class MainController extends AppController {
+
+    public function index(){
+
+        echo "Hello World!!!!!";
+
+    }
+
+}
